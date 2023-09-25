2ebffdce013236b397efc2aafaca93d59d115dd1
Efrain Giron <efrainbond@hotmail.com>
2023-09-25 02:32:29 -0600

    Agregados algunos productos, y una que otra prueba con estilos y angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'serv-mark';
+  name: string = '';
+  widthImg: number = 10;
+  imageUrl: string = '../assets/images/toy.jpg';
+  products: Product[] = [
+    {
+      name: 'El mejor juguete',
+      price: 565,
+      image: '../assets/images/toy.jpg',
+      category: 'Juguetes'
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'      
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: '../assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: '../assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: '../assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: '../assets/images/glasses.jpg'
+    }
+  ];
+
 }
