953f117da5e35935171b4795106230760ebdc3f9
Efrain Giron <efrainbond@hotmail.com>
2023-09-28 01:40:35 -0600

    Practica con BehaviorSubject

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
   private myShoppingCart: Product[] = [];
+  private myCart = new BehaviorSubject<Product[]>([]);
+
+  myCart$ = this.myCart.asObservable();
 
   constructor() { }
 
@@ -15,6 +19,7 @@ export class StoreService {
 
   addProduct(product: Product): void {
     this.myShoppingCart.push(product);
+    this.myCart.next(this.myShoppingCart);
   }
 
   getTotal(): number {
