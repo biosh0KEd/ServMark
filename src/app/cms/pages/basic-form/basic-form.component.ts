df34fa56d48cccd769fe8edbd2bd4ebe39b94400
Efrain Giron <efrainbond@hotmail.com>
2023-12-02 00:13:02 -0600

    Practicas con validaciones en FormControl.

--- src/app/cms/pages/basic-form/basic-form.component.ts
+++ src/app/cms/pages/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',    
@@ -7,7 +7,7 @@ import { FormControl } from '@angular/forms';
   styleUrl: './basic-form.component.sass'
 })
 export class BasicFormComponent implements OnInit {
-  nameField = new FormControl('');
+  nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
   emailField = new FormControl('');
   phoneField = new FormControl('');
   colorField = new FormControl('#000000');
@@ -16,6 +16,10 @@ export class BasicFormComponent implements OnInit {
 
   categoryField = new FormControl('category-2');
   tagField = new FormControl(['tag-1', 'tag-2']);
+
+  agreeField = new FormControl(false);
+  idTypeField = new FormControl('');
+  zoneField = new FormControl('');
 
   ngOnInit(): void {
     this.nameField.valueChanges.subscribe(value => {
@@ -27,4 +31,11 @@ export class BasicFormComponent implements OnInit {
     console.log(this.nameField.value);
   }
 
+  get isNameFieldValid(){
+    return this.nameField.touched && this.nameField.status === 'VALID';
+  }
+
+  get isNameFieldInvalid(){
+    return this.nameField.touched && this.nameField.status === 'INVALID';
+  }
 }
