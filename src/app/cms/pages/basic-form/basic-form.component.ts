5e903331cc43f6300bbc86ec0e721f4db28397d1
Efrain Giron <efrainbond@hotmail.com>
2023-12-02 01:42:54 -0600

    Agrupados varios FormControl en un FormGroup

--- src/app/cms/pages/basic-form/basic-form.component.ts
+++ src/app/cms/pages/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',    
@@ -7,19 +7,68 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrl: './basic-form.component.sass'
 })
 export class BasicFormComponent implements OnInit {
-  nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
-  emailField = new FormControl('');
-  phoneField = new FormControl('');
-  colorField = new FormControl('#000000');
-  dateField = new FormControl('');
-  numberField = new FormControl(123);
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    color: new FormControl('#000000'),
+    date: new FormControl(''),
+    number: new FormControl(123),
+    category: new FormControl('category-2'),
+    tag: new FormControl(['tag-1', 'tag-2']),
+    agree: new FormControl(false),
+    idType: new FormControl(''),
+    zone: new FormControl('')
+  });    
+ 
+  save(event : Event){
+    console.log(this.form.value);
+    console.log(event);
+  }
 
-  categoryField = new FormControl('category-2');
-  tagField = new FormControl(['tag-1', 'tag-2']);
+  get nameField(){
+    return this.form.get('name') ?? new FormControl();
+  }
 
-  agreeField = new FormControl(false);
-  idTypeField = new FormControl('');
-  zoneField = new FormControl('');
+  get emailField(){
+    return this.form.get('email') ?? new FormControl();
+  }
+
+  get phoneField(){
+    return this.form.get('phone') ?? new FormControl();
+  }
+
+  get colorField(){
+    return this.form.get('color') ?? new FormControl();
+  }
+
+  get dateField(){
+    return this.form.get('date') ?? new FormControl();
+  }
+
+  get numberField(){
+    return this.form.get('number') ?? new FormControl();
+  }
+
+  get categoryField(){
+    return this.form.get('category') ?? new FormControl();
+  }
+
+  get tagField(){
+    return this.form.get('tag') ?? new FormControl();
+  }
+
+  get agreeField(){
+    return this.form.get('agree') ?? new FormControl();
+  }
+
+  get idTypeField(){
+    return this.form.get('idType') ?? new FormControl();
+  }
+
+  get zoneField(){
+    return this.form.get('zone') ?? new FormControl();
+  }
 
   ngOnInit(): void {
     this.nameField.valueChanges.subscribe(value => {
@@ -31,6 +80,7 @@ export class BasicFormComponent implements OnInit {
     console.log(this.nameField.value);
   }
 
+
   get isNameFieldValid(){
     return this.nameField.touched && this.nameField.status === 'VALID';
   }
@@ -38,4 +88,5 @@ export class BasicFormComponent implements OnInit {
   get isNameFieldInvalid(){
     return this.nameField.touched && this.nameField.status === 'INVALID';
   }
+
 }
